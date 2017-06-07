17af576251268515824ac45bb570ca46010896da
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-07 18:06:37 +0200

    added currencies on sidebar

--- app/components/sidebar/index.js
+++ app/components/sidebar/index.js
@@ -1,17 +1,22 @@
 import * as React from 'react'
 import styles from './sidebar.scss'
+import { Link } from 'react-router-dom'
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+  let uniqueCurrencies = []
+  uniqueCurrencies = props.currencies
+  .map(currency => currency.value.content.currency)
+  .filter((item, i, ar) => ar.indexOf(item) === i)
+  .map((item, i) => (
+    <li key={i} className={styles.list_item}>
+      <Link to={'/currency/' + item} className={styles.item_link}>{item}</Link>
+    </li>
+  ))
   return (
     <aside className={styles.sidebar}>
       <h4 className={styles.sidebar_title}>Wallets</h4>
       <ul className={styles.sidebar_list}>
-        <li className={styles.list_item}>
-          <a className={styles.item_link}>Ssbcoin</a>
-        </li>
-        <li className={styles.list_item}>
-          <a className={styles.item_link}>Eco</a>
-        </li>
+        {uniqueCurrencies}
       </ul>
     </aside>
   )
